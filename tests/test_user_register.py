a6ac8f15d9e37416701d8e488e1729f88683b43d
Sodiqmirzo <mr.sodiqmirzosattorov17@gmail.com>
2022-05-12 16:43:05 +0500

    create user test

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,30 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from datetime import datetime
 
 
 class TestUserRegister(BaseCase):
+    def setup(self):
+        base_part = "learnqa"
+        domain = "gmail.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}@{random_part}.{domain}"
+
+    def test_create_user_successfully(self):
+        data = {
+            'password': '12345',
+            'username': 'test_user',
+            'firstName': 'test',
+            'lastName': 'user',
+            'email': self.email
+        }
+
+        response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, 'id')
+
     def test_create_user_with_existing_email(self):
         email = 'sodikmirzo@gmail.com'
         data = {
@@ -19,6 +40,6 @@ class TestUserRegister(BaseCase):
         print(f"{response.status_code=}")
         print(f"{response.content=}")
 
-        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        Assertions.assert_code_status(response, 400)
         assert response.content.decode(
             "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
